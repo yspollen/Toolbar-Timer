30008f8c128d33fb0909b970cbd56cc1f975871a
yspollen <yspollen@purdue.edu>
2018-06-23 17:37:49 -0400

    popup window made more interactive & added time's up alarm

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,14 @@
-var countingDown = false;
+//Hide certain elements
 $('#alert').hide();
+$("#b2").hide();
+$("#b3").hide();
 
-var t; //variable for setInterval
-var destination;
+var t; //object for setInterval
+var snd; //alert sound
+var destinationTime; //destination time
+var startTime; //start time
+var countingDown = false; //Status of timer
+
 
 $("#b1").click(function(){
     startCountDown();
@@ -12,79 +18,119 @@ $("#b2").click(function(){
     cancelCountDown();
 });
 
+$("#b3").click(function(){
+    okay();
+});
+
+//Start Button
 function startCountDown() {
+    $("#b3").hide();
+
     var validTime = true;
+
     if (!countingDown){
         countingDown = true;
-        //Start Count Down
-        //Get the start time
-        var startTime = new Date().getTime();
 
-        //Set the destination time
-        var hours = $('#hour').val();
-        var minutes = $('#minute').val();
-        var seconds = $('#second').val();
+        //START COUNTDOWN
+        //Get the start time
+        startTime = new Date().getTime();
+
+        //get inputs
+        var hour = $('#hour').val();
+        var minute = $('#minute').val();
+        var second = $('#second').val();
         
         //Check for invalid inputs
-        if (minutes > 59 || seconds > 59){
+        if (minute > 59 || second > 59){
             validTime = false;
         }
-        else if (hours == 0 && minutes == 0 && seconds == 0){
+        else if (hour == 0 && minute == 0 && second == 0){
             validTime = false;
         }
 
-        //Show alert message if inputs are invalid
+        //Show alert message if the inputs are invalid
         if (!validTime){
             $('#display').text("0h 0m 0s");
             $('#alert').show();
             countingDown = false;
         }
         else{
-            chrome.browserAction.setBadgeText({text: 'ON'});
+            //hide and show certain elements
+            $("#b1").hide();
+            $("#b2").show();
+            $("#b3").hide();
             $('#alert').hide();
             $('#form').hide();
-            destinationTime = seconds*(1000) + minutes*(1000*60) + hours*(1000*3600) + startTime;
+            
+            destinationTime = second*(1000) + minute*(1000*60) + hour*(1000*3600) + startTime + 1000;
 
             //Decrement time by 1 second
             updateTime();
             t = setInterval(function() {
                 updateTime();
-            }, 500);
+            });
         }
     }
 }
 
+
+//Cancel Button
 function cancelCountDown() {
     if (countingDown){
         countingDown = false;
-        $('#form').show();
+        clearInterval(t); //clear timer
+
         $('#display').text("0h 0m 0s");
-        chrome.browserAction.setBadgeText({text: 'OFF'});
-        clearInterval(t);
+
+        //hide and show certain elements
+        $('#form').show();
+        $("#b2").hide();
+        $("#b1").show();
     }
 }
 
+//Okay Button
+function okay(){
+    //stop alert
+    snd.pause();
+    snd.currentTime = 0;
+
+    $('#display').text("0h 0m 0s");
+
+    //hide and show certain elements
+    $("#b3").hide();
+    $("#b1").show();
+    $('#form').show();
+}
+
 function updateTime(){
-    //The kinetic variable
+    //the time that keeps track of now
     var now = new Date().getTime();
 
-    //Find distance from now to destinationTime
+    //find distance from now to destinationTime
     var distance = destinationTime - now;
 
-    //Find days, hours, minutes, and seconds
+    //find days, hours, minutes, and seconds
     var hours = Math.floor((distance%(1000*3600*24))/(1000*3600));
     var minutes = Math.floor((distance%(1000*3600))/(1000*60));
     var seconds = Math.floor((distance%(1000*60))/1000);
 
-    // Output the result in an element with id="display"
+    //output the result in an element with id="display"
     $('#display').text(hours + "h " + minutes + "m " + seconds + "s ");
 
-    // If the count down is over, write some text 
+    // if the count down is over, alert "Time's Up!"
     if (distance < 0) {
-        clearInterval(t);
-        $('#display').text("Time's Up!");
         countingDown = false;
-        chrome.browserAction.setBadgeText({text: 'OFF'});
-        $('#form').show();
+        clearInterval(t); //clear timer
+
+        $('#display').text("Time's Up!");
+
+        //hide and show certain elements
+        $("#b2").hide();
+        $("#b3").show();
+        
+        //alert audio
+        snd = new Audio('/Louie\ Louie.mp3');
+        snd.play();
     }
 }
